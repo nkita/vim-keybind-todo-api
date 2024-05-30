d6ce213d0a7463d5361613f4b36620ac92de43cb
nkita <nkitam7@gmail.com>
2024-05-30 21:41:07 +0900

    feat: 認証機能追加（Auth0）

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,8 +1,10 @@
 import { responseJson } from '@/lib/response'
 import { select } from '@/db/user'
+import { getSession } from '@auth0/nextjs-auth0';
 
 export const GET = async () => {
-    const user = await select({})
+    const { user } = await getSession();
+    // const user = await select({})
     return responseJson(200, user)
 }
 export const POST = async () => responseJson(405)
