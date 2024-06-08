3a3ce3ae77cc104ca028b68dda343568d5444387
nkita <nkitam7@gmail.com>
2024-06-08 15:02:02 +0900

    feat: TodoList DBの追加

--- src/db/todolist.ts
+++ src/db/todolist.ts
@@ -1,5 +1,41 @@
 import prisma from "@/db/prisma";
+import { randomUUID } from "crypto";
 
 export const select = async (where: any) => {
     return await prisma.todoList.findMany({ where: where })
 }
+
+/**
+ * TodoList作成
+ */
+export const create = async (
+    name: string,
+    user_id: string
+) => await prisma.todoList.create({
+    data: {
+        id: randomUUID(),
+        name: name,
+        created_at: new Date(),
+        updated_at: new Date(),
+        userId: user_id,
+    }
+})
+
+/**
+ * TodoList更新
+ */
+export const update = async (
+    id: string,
+    user_id: string,
+    name: string
+) => await prisma.todoList.update({
+    where: {
+        id: id,
+        userId: user_id
+    },
+    data: {
+        name: name,
+        updated_at: new Date(),
+    }
+})
+
