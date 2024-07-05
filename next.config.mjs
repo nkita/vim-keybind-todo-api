82cf31e64f531eb55c837b77167397786248df5a
nkita <nkitam7@gmail.com>
2024-07-05 22:03:45 +0900

    feat: cors対応

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                source: "/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    // { key: "Access-Control-Allow-Origin", value: process.env.CORS_ORIGIN },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Access-Control-Allow-Headers", value: "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
                 ]
             }
-        ]
-    }
-};
+        ];
+    },
+}
 
 export default nextConfig;
